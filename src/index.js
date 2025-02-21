03d71ef889ee9fdd6b502ea53ba2698876d4c793
Yanchuk Andrey <orpiy@mail.ru>
2025-02-22 01:23:25 +0700

    add: add new file src/index.js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// src/index.js
+// import util from "util"; // Для отображения уровня вложенности
+// console.log(util.inspect(getMutualFriends(user1, user2), { depth: 5, colors: true }));
+/*-----------------------------------------------------*/
