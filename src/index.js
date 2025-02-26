5425759fe8a1acc88100a78c1097f22488cf8079
Yanchuk Andrey <orpiy@mail.ru>
2025-02-26 23:01:17 +0700

    feat: implement normalize function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@
 // import util from "util"; // Для отображения уровня вложенности
 // console.log(util.inspect(getMutualFriends(user1, user2), { depth: 5, colors: true }));
 /*-----------------------------------------------------*/
+export const normalize = (countries) => {
+    if (!Array.isArray(countries) || countries.length === 0)
+        throw new Error("countries must be type data 'array'!");
+    return countries.reduce((acc, { country, name }) => {
+        const normalizeCountry = country.trim().toLowerCase();
+        const normalizeName = name.trim().toLowerCase();
+        if (!acc[normalizeCountry]) acc[normalizeCountry] = [];
+        acc[normalizeCountry].push(normalizeName);
+        acc[normalizeCountry].sort();
+        return acc;
+    }, {});
+};
+/*-----------------------------------------------------*/
